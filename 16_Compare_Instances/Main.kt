8fb2ce2896cd394faf7ea6a970b3b252ad37ac9e
meilan <meilanulfia@gmail.com>
2026-09-24 19:35:19 +0700

    Compare instances
    Add a data class User and comparison examples in main.

---
+++ 16_Compare_Instances/Main.kt
@@ -0,0 +1,13 @@
+data class User(val name: String, val id: Int)
+
+fun main() {
+    val user = User("Alex", 1)
+    val secondUser = User("Alex", 1)
+    val thirdUser = User("Max", 2)
+
+    // Membandingkan User dengan User kedua
+    println("user == secondUser: ${user == secondUser}")
+
+    // Membandingkan User dengan User ketiga
+    println("user == thirdUser: ${user == thirdUser}")
+}
